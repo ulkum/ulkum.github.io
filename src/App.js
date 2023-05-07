6b88c69f193ff5b3a61bc160042466a1de7fba2b
Konstantin <79552566+ulkum@users.noreply.github.com>
2023-05-07 19:47:34 +0900

    add alert for button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  const styles = StyleSheet.cre;
-
   return (
-    <div className="App" style={styles}>
-      <header className="App-header">Helo</header>
+    <div className="App">
+      <button onClick={() => alert("Попался рак!")} className="button">
+        Push me
+      </button>
     </div>
   );
 };
